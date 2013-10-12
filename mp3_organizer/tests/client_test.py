3651e163d548501d88599d01280986dbc3fde42b
Ofir <ofirbrukner@gmail.com>
2013-10-12 15:31:27 +0300

    Added parametrization to client tests.

--- mp3_organizer/tests/client_test.py
+++ mp3_organizer/tests/client_test.py
@@ -1,16 +1,25 @@
 __author__ = 'Halti'
 
 import pytest
+import time
 from test_consts import *
 from mp3_organizer.clients.gracenote.gracenote_client import *
+from mp3_organizer.clients.amazon.amazon_client import *
 
 
 @pytest.fixture
-def test_client():
+def gracenote_test_client():
     return GracenoteClient()
 
 
-@pytest.mark.usefixtures("test_client")
+@pytest.fixture
+def amazon_test_client():
+    # Wait so the service won't be overflowed with requests.
+    time.sleep(1)
+    return AmazonClient()
+
+
+@pytest.mark.parametrize("test_client", [amazon_test_client(), gracenote_test_client()])
 class TestClient:
     """
     Tests for the Gracenote client.
