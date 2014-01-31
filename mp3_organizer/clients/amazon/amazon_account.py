90f942710c93259b8768900b35d34f3f16651a37
Ofir <ofirbrukner@gmail.com>
2014-01-31 16:58:06 +0200

    Fixed the .gitignore file.

--- mp3_organizer/clients/amazon/amazon_account.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Halti'
-
-"""
-Account details for the Amazon API.
-"""
-
-ACCESS_KEY = 'AKIAI6YSI466WE2NMJKQ'
-SECRET_KEY = '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'
-ASSOCIATE_TAG = 'mp3org09-20'
