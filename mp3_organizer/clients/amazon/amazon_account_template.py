52ef568814db19589fb6323805784f1bddac3e35
Ofir <ofirbrukner@gmail.com>
2014-10-22 00:20:30 +0300

    Added a template for the amazon_account.py file

---
+++ mp3_organizer/clients/amazon/amazon_account_template.py
@@ -0,0 +1,14 @@
+"""
+Account details for the Amazon API.
+
+In order for the Amazon client to work,
+You'll need to change this file's name to 'amazon_account.py' and fill in the following details:
+Access key, secret key & associate tag.
+You can get all of these by creating an AWS account (and a little help from Google).
+
+Good luck!
+"""
+
+ACCESS_KEY = ''
+SECRET_KEY = ''
+ASSOCIATE_TAG = ''
